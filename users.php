1117b86382336a6d383f451f8fb2f5f96a92d833
Jean FRUITET <jean.fruitet@free.fr>
2024-03-20 11:35:51 +0100

    Simplification
    Signed-off-by: Jean FRUITET <jean.fruitet@free.fr>

--- users.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // A placer ne tête de toutes les pages nécessitant une connexion authentifiée
-    // On démarre une nouvelle session
-    include("./php/include/session.php");
-    include("users.html");
-?>
-
